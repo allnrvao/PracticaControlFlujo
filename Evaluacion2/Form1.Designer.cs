642c11c78ac5353f226c10c6447a3442b688776b
allnrvao <aracuna@uamv.edu.ni>
2024-09-12 13:00:39 -0600

    Completed PracticaControlFlujo exercise

---
+++ Evaluacion2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Evaluacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbSalida = new System.Windows.Forms.Label();
+            this.tbNIck = new System.Windows.Forms.TextBox();
+            this.tbContra = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingreso de Usuarios";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese nick";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese Contraseña";
+            // 
+            // lbSalida
+            // 
+            this.lbSalida.AutoSize = true;
+            this.lbSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSalida.Location = new System.Drawing.Point(93, 323);
+            this.lbSalida.Name = "lbSalida";
+            this.lbSalida.Size = new System.Drawing.Size(92, 32);
+            this.lbSalida.TabIndex = 3;
+            this.lbSalida.Text = "label4";
+            // 
+            // tbNIck
+            // 
+            this.tbNIck.Location = new System.Drawing.Point(286, 104);
+            this.tbNIck.Name = "tbNIck";
+            this.tbNIck.Size = new System.Drawing.Size(274, 26);
+            this.tbNIck.TabIndex = 4;
+            // 
+            // tbContra
+            // 
+            this.tbContra.Location = new System.Drawing.Point(286, 189);
+            this.tbContra.Name = "tbContra";
+            this.tbContra.Size = new System.Drawing.Size(274, 26);
+            this.tbContra.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(607, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 108);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbContra);
+            this.Controls.Add(this.tbNIck);
+            this.Controls.Add(this.lbSalida);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbSalida;
+        private System.Windows.Forms.TextBox tbNIck;
+        private System.Windows.Forms.TextBox tbContra;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
